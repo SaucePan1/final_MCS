bcedf17f6d2631defbe29655b2d3d0c10b7aa8d1
Bernat Gilboy Sopena <>
2019-06-12 16:42:37 +0200

    push

--- resources/project/get_pc_over_t.m
+++ resources/project/get_pc_over_t.m
@@ -4,16 +4,14 @@ c= 0.2;
 b = b_c_ratio*c;
 w= 0.01;
 dt = 0.01;
-Bs = 1;
-delta = 1
-Bw = delta*Bs;
+Bs = 5;
 n=100;
 %graph neigh
 k= 4;
 
 %sim parameters
 T= 1000;
-max_runs = 20;
+max_runs = 2;
 
 
 d_ratios = [0.001, 0.05, 0.1, 0.15,0.25];
@@ -22,11 +20,11 @@ k= 4; % num of neighbours
 %create graph (sparse matrix)
 %create cycle
 
-og_graph = getGraph("rrg", 100, k);
+og_graph = getGraph("rrg", n, k);
 
-Bs*dt*n
+deaths= Bs*dt*n
 %
-pc_t_all_sim= zeros(max_runs,T, length(b_ratios));
+pc_t_all_sim= zeros(max_runs,T, length(d_ratios));
 for count =1:length(d_ratios)
     delta = d_ratios(count)
     Bw = delta*Bs;
